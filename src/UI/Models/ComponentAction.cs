3e9395c8d9cf653ad88391567b7d1a459a81e014
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-10-21 15:02:39 +0300

    Version 1.0.0-beta.1

---
+++ src/UI/Models/ComponentAction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Netopes.Blazor.UI.Models
+{
+    public enum ComponentActionTypes
+    {
+        Route,
+        Event
+    }
+    
+    public class ComponentAction
+    {
+        public ComponentActionTypes Type { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Route { get; set; }
+        public EventCallback<string> Event { get; set; }
+    }
+}
