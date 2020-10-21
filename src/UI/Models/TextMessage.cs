560a4b7db72626dac988a8ae3a762508a7454f36
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-10-21 22:09:46 +0300

    Version 1.0.0-beta.2

---
+++ src/UI/Models/TextMessage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Netopes.Blazor.UI.Models
+{
+    public class TextMessage
+    {
+        public string Text { get; set; }
+        public bool IsError { get; set; } = true;
+        
+        public TextMessage(string text, bool isError = true)
+        {
+            Text = text;
+            IsError = isError;
+        }
+        
+        public MarkupString Message => (MarkupString) Text;
+    }
+}
