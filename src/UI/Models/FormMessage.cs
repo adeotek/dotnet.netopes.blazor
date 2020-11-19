062bc03a3080154e39757765f3a0643c9268828e
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-11-19 19:22:10 +0200

    v1.0.1 - Breadcrumbs changes (optimization) - ModalForm component added - FormMessage helper class added - RequiredGuidValidationAttribute model validator added

---
+++ src/UI/Models/FormMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Netopes.Blazor.UI.Models
+{
+    public class FormMessage
+    {
+        public string Message { get; set; }
+        public bool IsError { get; set; } = true;
+
+        public FormMessage(string message, bool isError = true)
+        {
+            Message = message;
+            IsError = isError;
+        }
+    }
+}
